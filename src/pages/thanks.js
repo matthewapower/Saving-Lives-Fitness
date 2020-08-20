3386d9c438142c078ff369b974c8a96d7ce03dc4
matthewapower <matthewallenpower@gmail.comm>
2020-08-20 09:38:13 -0400

    Initiate subscribe form

---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import SubscribeForm from "../components/subscribeForm"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <SubscribeForm />
+  </Layout>
+)
+
+export default NotFoundPage
