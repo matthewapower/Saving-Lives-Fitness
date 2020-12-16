d90b1f7bcf8ef4ded199b7fda2064201a0102868
matthewapower <matthewallenpower@gmail.comm>
2020-12-16 18:49:30 -0500

    Fix nav on mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -14,13 +14,13 @@ const Header = props => {
     if (props.navOpen) {
       setLogoPos("")
     } else if (props.home) {
-      setLogoPos("translateY(70%) scale(1.6)")
+      setLogoPos("translateY(90%) scale(1.6)")
     }
   }, [props.navOpen, setLogoPos])
 
   return (
     <>
-      <header className="grid absolute top-0 inset-x-0 items-center justify-center grid-cols-5">
+      <header className="grid absolute top-0 inset-x-0 items-center justify-center grid-cols-1 md:grid-cols-5">
         <div class="order-1 flex col-span-2 justify-end">
           {props.links.map((l, i) => {
             if (i + 1 <= props.links.length / 2)
@@ -38,7 +38,7 @@ const Header = props => {
         </div>
         <Link
           to="/"
-          className="p-4 z-50 order-2"
+          className="p-4 z-50 md:order-2 mx-auto w-1/2 md:w-full"
           css={css`
             transform: ${logoPos};
             max-width: 400px;
